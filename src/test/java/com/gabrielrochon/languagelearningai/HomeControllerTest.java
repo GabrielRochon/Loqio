3b82fd043dc280165d1739aa13f66ad986c1c25c
Gabriel Rochon <rochon.gabriel1@gmail.com>
2025-12-22 15:29:11 -0500

    Add basic unit test

---
+++ src/test/java/com/gabrielrochon/languagelearningai/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package com.gabrielrochon.languagelearningai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class HomeControllerTest
+{
+	@Autowired
+	private HomeController homeController;
+
+	@Test
+	public void GetHelloFromRootEndpoint()	
+	{
+		String response = homeController.home();
+		assertThat(response).isEqualTo("Hello, World!");
+	}
+}
