6c8966e596509e7d4a46f710637f8bdac94b668f
Gabriel Rochon <rochon.gabriel1@gmail.com>
2025-12-22 12:57:49 -0500

    Binplace project skeleton, generated with https://start.spring.io/

---
+++ src/main/java/com/gabrielrochon/languagelearningai/LanguagelearningaiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielrochon.languagelearningai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguagelearningaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguagelearningaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielrochon/languagelearningai/LanguagelearningaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielrochon.languagelearningai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguagelearningaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
